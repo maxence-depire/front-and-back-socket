7a6d28af5aa4aa19f2150d7e66eb5b384f507097
Max__La__Menace <maxence.depire@gmail.com>
2024-02-14 11:03:35 +0100

    lil update on server, and client.

--- back/main.py
+++ back/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request,jsonify
 from flask_socketio import SocketIO,emit
 from flask_cors import CORS
+import time
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -33,4 +34,5 @@ def disconnected(data=None):
     emit("disconnect",f"user {request.sid} disconnected",broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True,port=5000)
+    print("run")
+    socketio.run(app, debug=False,port=5000)
